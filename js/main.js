ac7951a8689d79a25730b5a4bf8a3ac9cc53c618
valentinzberea <valentin.zberea@gmail.com>
2014-04-09 15:48:13 +0300

    Fix indenting

--- js/main.js
+++ js/main.js
@@ -1,92 +1,92 @@
 (function(){
     var configA = {
         context: 'A',
-baseUrl: '/bower_components/',
-paths: {
-    'jquery': 'jquery-1.7.1/jquery',
-'underscore': 'underscore-1.4.4/underscore',
-'backbone': 'backbone-1.0.0/backbone'
-},
-shim: {
-    jquery: {
-        exports: '$'
-    },
-underscore: {
-    exports: '_'
-},
-backbone: {
-    deps: ['jquery', 'underscore'],
-exports: 'Backbone'
-}
-},
-    waitSeconds: 30
+        baseUrl: '/bower_components/',
+        paths: {
+                'jquery': 'jquery-1.7.1/jquery',
+                    'underscore': 'underscore-1.4.4/underscore',
+                        'backbone': 'backbone-1.0.0/backbone'
+                        },
+        shim: {
+            jquery: {
+                exports: '$'
+            },
+            underscore: {
+                exports: '_'
+            },
+            backbone: {
+                deps: ['jquery', 'underscore'],
+                exports: 'Backbone'
+            }
+        },
+        waitSeconds: 30
     };
-var configB = {
-    context: 'B',
-    baseUrl: '/bower_components/',
-    paths: {
-        'jquery': 'jquery-1.8.3/jquery',
-        'underscore': 'underscore-1.4.2/underscore',
-        'backbone': 'backbone-0.9.1/backbone'
-    },
-    shim: {
-        jquery: {
-            exports: '$'
+    var configB = {
+        context: 'B',
+        baseUrl: '/bower_components/',
+        paths: {
+            'jquery': 'jquery-1.8.3/jquery',
+            'underscore': 'underscore-1.4.2/underscore',
+            'backbone': 'backbone-0.9.1/backbone'
         },
-        underscore: {
-            exports: '_'
+        shim: {
+            jquery: {
+                exports: '$'
+            },
+            underscore: {
+                exports: '_'
+            },
+            backbone: {
+                deps: ['jquery', 'underscore'],
+                exports: 'Backbone'
+            }
         },
-        backbone: {
-            deps: ['jquery', 'underscore'],
-            exports: 'Backbone'
-        }
-    },
-    waitSeconds: 30
-};
+        waitSeconds: 30
+    };
 
-var requireA = requirejs.config(configA);
-var requireB = requirejs.config(configB);
+    var requireA = requirejs.config(configA);
+    var requireB = requirejs.config(configB);
 
-var logToResults = function(message){
-    results = $('#results').text();
-    results = results + '\n' + message;
-    $('#results').text(results);
-};
-var logVersions = function(){
-    logToResults('jQuery v.' + $.fn.jquery);
-    logToResults('underscore v.' + _.VERSION);
-    logToResults('backbone v.' + Backbone.VERSION);
-};
-var undefCommonLibraries = function(require){
-    require.undef('jquery');
-    require.undef('underscore');
-    require.undef('backbone');
-}
+    var logToResults = function(message){
+        results = $('#results').text();
+        results = results + '\n' + message;
+        $('#results').text(results);
+    };
+    var logVersions = function(){
+        logToResults('jQuery v.' + $.fn.jquery);
+        logToResults('underscore v.' + _.VERSION);
+        logToResults('backbone v.' + Backbone.VERSION);
+    };
+    var undefCommonLibraries = function(require){
+        require.undef('jquery');
+        require.undef('underscore');
+        require.undef('backbone');
+    }
 
-requireA(['require', 'backbone'], function(require){
+    requireA(['require', 'backbone'], function(require){
 
-    logToResults('Initial versions:');
-    logVersions();
+        logToResults('Initial versions:');
+        logVersions();
 
-    require(['jquery'], function($){
+        require(['jquery'], function($){
 
-        $('#change_to_a').click(function(){
-            undefCommonLibraries(requireA);
-            requireA(['jquery', 'backbone'], function(){
-                logToResults('Versions on A:');
-                logVersions();
+            $('#change_to_a').click(function(){
+                undefCommonLibraries(requireA);
+                requireA(['jquery', 'backbone'], function(){
+                    logToResults('Versions on A:');
+                    logVersions();
+                });
             });
-        });
-        $('#change_to_b').click(function(){
-            undefCommonLibraries(requireB);
-            requireB(['jquery', 'backbone'], function(){
-                logToResults('Versions on B:');
-                logVersions();
+            $('#change_to_b').click(function(){
+                undefCommonLibraries(requireB);
+                requireB(['jquery', 'backbone'], function(){
+                    logToResults('Versions on B:');
+                    logVersions();
+                });
             });
+
         });
 
     });
-
-});
 
 })();
