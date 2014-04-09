b00971888c16a1dcaadae2b9a05ea208bb552902
valentinzberea <valentin.zberea@gmail.com>
2014-04-09 15:41:27 +0300

    Fix the shims after changing require context

--- js/main.js
+++ js/main.js
@@ -1,85 +1,92 @@
 (function(){
-  var configA = {
-    context: 'A',
-    baseUrl: '/bower_components/',
-    paths: {
-      'jquery': 'jquery-1.7.1/jquery',
-      'underscore': 'underscore-1.4.4/underscore',
-      'backbone': 'backbone-1.0.0/backbone'
-    },
-    shim: {
-      jquery: {
+    var configA = {
+        context: 'A',
+baseUrl: '/bower_components/',
+paths: {
+    'jquery': 'jquery-1.7.1/jquery',
+'underscore': 'underscore-1.4.4/underscore',
+'backbone': 'backbone-1.0.0/backbone'
+},
+shim: {
+    jquery: {
         exports: '$'
-      },
-      underscore: {
-        exports: '_'
-      },
-      backbone: {
-        deps: ['jquery', 'underscore'],
-        exports: 'Backbone'
-      }
     },
+underscore: {
+    exports: '_'
+},
+backbone: {
+    deps: ['jquery', 'underscore'],
+exports: 'Backbone'
+}
+},
     waitSeconds: 30
-  };
-  var configB = {
+    };
+var configB = {
     context: 'B',
     baseUrl: '/bower_components/',
     paths: {
-      'jquery': 'jquery-1.8.3/jquery',
-      'underscore': 'underscore-1.4.2/underscore',
-      'backbone': 'backbone-0.9.1/backbone'
+        'jquery': 'jquery-1.8.3/jquery',
+        'underscore': 'underscore-1.4.2/underscore',
+        'backbone': 'backbone-0.9.1/backbone'
     },
     shim: {
-      jquery: {
-        exports: '$'
-      },
-      underscore: {
-        exports: '_'
-      },
-      backbone: {
-        deps: ['jquery', 'underscore'],
-        exports: 'Backbone'
-      }
+        jquery: {
+            exports: '$'
+        },
+        underscore: {
+            exports: '_'
+        },
+        backbone: {
+            deps: ['jquery', 'underscore'],
+            exports: 'Backbone'
+        }
     },
     waitSeconds: 30
-  };
+};
 
-  var requireA = requirejs.config(configA);
-  var requireB = requirejs.config(configB);
+var requireA = requirejs.config(configA);
+var requireB = requirejs.config(configB);
 
-  var logToResults = function(message){
+var logToResults = function(message){
     results = $('#results').text();
     results = results + '\n' + message;
     $('#results').text(results);
-  };
-  var logVersions = function($, _, Backbone){
+};
+var logVersions = function(){
     logToResults('jQuery v.' + $.fn.jquery);
     logToResults('underscore v.' + _.VERSION);
     logToResults('backbone v.' + Backbone.VERSION);
-  };
+};
+var undefCommonLibraries = function(require){
+    require.undef('jquery');
+    require.undef('underscore');
+    require.undef('backbone');
+}
 
-  requireA(['require', 'jquery', 'underscore', 'backbone'], function(require, $, _, Backbone){
+requireA(['require', 'backbone'], function(require){
 
     logToResults('Initial versions:');
-    logVersions($, _, Backbone);
+    logVersions();
 
     require(['jquery'], function($){
 
-      $('#change_to_a').click(function(){
-        requireA(['jquery', 'underscore', 'backbone'], function($, _, Backbone){
-          logToResults('Versions on A:');
-          logVersions($, _, Backbone);
+        $('#change_to_a').click(function(){
+            undefCommonLibraries(requireA);
+            requireA(['jquery', 'backbone'], function(){
+                logToResults('Versions on A:');
+                logVersions();
+            });
         });
-      });
-      $('#change_to_b').click(function(){
-        requireB(['jquery', 'underscore', 'backbone'], function($, _, Backbone){
-          logToResults('Versions on B:');
-          logVersions($, _, Backbone);
+        $('#change_to_b').click(function(){
+            undefCommonLibraries(requireB);
+            requireB(['jquery', 'backbone'], function(){
+                logToResults('Versions on B:');
+                logVersions();
+            });
         });
-      });
 
     });
 
-  });
+});
 
 })();
